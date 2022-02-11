80fc6844a2de4af8b8ee7730aba8a376b1695c8c
StephenK <kotiangsteve@gmail.com>
2022-02-11 11:40:23 -0600

    Update Task0.py

--- p0/Task0.py
+++ p0/Task0.py
@@ -3,6 +3,7 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
 import csv
+
 with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
